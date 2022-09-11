e9aad3803e17163a91b132e3686af8df654f948c
Jorge Martin <jorge.martin@pulselive.com>
2022-09-11 13:03:39 +0200

    Moved to rememberSaveable to retain app's state during configuration changes

--- app/src/main/java/com/example/compose04state/ui/widget/WaterCounter.kt
+++ app/src/main/java/com/example/compose04state/ui/widget/WaterCounter.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,7 +17,7 @@ import com.example.compose04state.ui.theme.Compose04StateTheme
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count by remember { mutableStateOf(0) }
+    var count by rememberSaveable { mutableStateOf(0) }
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
             Text(
